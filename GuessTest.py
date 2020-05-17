ebe458061e1ed064e2046a2eb59029d8b19f03d0
Florian Schwarzinger <fschwarzinger@olin.edu>
2020-05-16 19:53:33 -0600

    Updated GuessTest

--- GuessTest.py
+++ GuessTest.py
@@ -1,10 +1,19 @@
 import json
+import pyautogui
+import time
 
 with open("updated_list.json") as json_file:
     words = json.load(json_file)
 
-print(words['10'])
-print(words['16'])
-print(words['17'])
-print(words['18'])
-print(words['19'])
+in_str = "1234567890"
+spaces = str(len(in_str))
+
+time.sleep(4)
+
+count = 0
+for word in words[spaces]:
+    count += 1
+    pyautogui.typewrite(word + '\n')
+    if count == 4:
+        time.sleep(2.25)
+        count = 0
